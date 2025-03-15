6923d28e21637f56add9d4f17ecaee3f60f4d4f8
Matt Stein <matt.m.stein@gmail.com>
2025-03-15 17:22:00 -0400

    Adds multiple swatches
    It now can have as many swatches as one deems sane, I added a button that can add another swatch using JavaScript. It also now starts with 3 swatches.

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ This simple web app makes it easy to design 2 color gradient backgrounds.
 [X] - Figure out the basic JavaScript and HTML
 [X] - Make a well styled navigation menu
 [X] - Design the CSS for the basic website
+[X] - Make it so you can add multiple swatches
+[ ] - Make it so you can choose between radial and linear gradient
+[ ] - Design it so you can remove swatches
 [ ] - Design an animation for toggling the nav menu
 [ ] - Polish and publish
 
--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const direction = document.getElementById('direction');
-const firstColor = document.getElementById('first-color');
-const secondColor = document.getElementById('second-color');
+// const firstColor = document.getElementById('first-color');
+// const secondColor = document.getElementById('second-color');
 const switchIcon = document.getElementById('switch-icon');
+const swatchContainer = document.getElementById('swatch-container');
 const nav = document.querySelector('nav');
+const button = document.getElementById('btn');
 
 console.log(direction);
-console.log(firstColor);
-console.log(secondColor);
+
 console.log(switchIcon);
+console.log(swatchContainer);
 
 let menuDisplay = true;
+
+let colors = [
+    "#7be383",
+    '#5cf2de',
+    '#ffffff'
+];
 
 let dir = '90deg';
 let color1 = "#7be383";
@@ -29,13 +37,29 @@ switchIcon.addEventListener('click', () => {
     }
 });
 
-function changeBackground(dir, mainColor, secondColor) {
+/* function changeBackground(dir, mainColor, secondColor) {
     document.body.style.backgroundImage = `linear-gradient(${dir}, ${mainColor}, ${secondColor})`;
     direction.style.borderColor = mainColor;
     direction.style.backgroundColor = secondColor;
+} */
+
+function changeBackground(dir, swatchesColors) {
+    let gradient = `linear-gradient(${dir}`;
+
+    swatchesColors.forEach((color) => {
+        gradient += `, ${color}`;
+    });
+
+    gradient += `)`;
+
+    console.log(gradient)
+    
+    document.body.style.backgroundImage = gradient;
+    // direction.style.borderColor = mainColor;
+    // direction.style.backgroundColor = secondColor;
 }
 
-firstColor.addEventListener('change', (event) => {
+/* firstColor.addEventListener('change', (event) => {
     color1 = event.target.value;
     changeBackground(dir, color1, color2);
 });
@@ -43,13 +67,41 @@ firstColor.addEventListener('change', (event) => {
 secondColor.addEventListener('change', (event) => {
     color2 = event.target.value;
     changeBackground(dir, color1, color2);
-});
+}); */
 
 direction.addEventListener('input', (event) => {
     dir = `${event.target.value}deg`;
-    changeBackground(dir, color1, color2);
+    changeBackground(dir, colors);
 });
 
-changeBackground(dir, color1, color2);
-firstColor.value = color1;
-secondColor.value = color2;
+changeBackground(dir, colors);
+
+function createColorSwatchesElements(color) {
+    const swatch = document.createElement(`input`);
+    swatch.value = color;
+    swatch.classList.add('swatch');
+    swatch.type = 'color';
+    swatchContainer.appendChild(swatch);
+    return swatch;
+}
+
+colors.forEach((color, index) => {
+    addToEachSwatch(color, index);
+});
+
+function addToEachSwatch(color, index) {
+    swatchElement = createColorSwatchesElements(color);
+    swatchElement.addEventListener('change', (event) => {
+        colors[index] = event.target.value;
+        changeBackground(dir, colors);
+    });
+}
+
+button.addEventListener('click', () =>{
+    console.log(colors.length);
+    colors.push("#0000FF");
+    console.log(colors.length);
+    addToEachSwatch(colors[colors.length - 1], colors.length - 1);
+    
+    changeBackground(dir, colors);
+});
